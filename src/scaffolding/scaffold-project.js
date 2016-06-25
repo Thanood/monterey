08b4bc10e76246772c2b517e4e7aa33e2f694106
Jeroen <Jeroenvinke17@gmail.com>
2016-06-25 19:54:35 +0200

    chore(setup): try out fs abstraction by loading file from aurelia-cli

--- src/scaffolding/scaffold-project.js
+++ src/scaffolding/scaffold-project.js
@@ -2,15 +2,23 @@ import {inject, NewInstance}  from 'aurelia-framework';
 import {DialogController}     from 'aurelia-dialog';
 import {ValidationController} from 'aurelia-validation';
 import {ValidationRules}      from 'aurelia-validatejs';
+import {Fs}                   from '../shared/abstractions/fs';
 
-@inject(DialogController, NewInstance.of(ValidationController))
+@inject(DialogController, NewInstance.of(ValidationController), Fs)
 export class ScaffoldProject {
-  constructor(dialog, validator) {
+  constructor(dialog, validator, fs) {
     this.dialog = dialog;
     this.validator = validator;
+    this.fs = fs;
   }
 
-  attached() {
+  async activate() {
+    this.questions = JSON.parse(await this.fs.readFile('node_modules/aurelia-cli/lib/commands/new/new-application.json'));
+
+    alert(`Loaded ${this.questions.activities.length} questions from aurelia-cli`);
+  }
+
+  async attached() {
     ValidationRules
     .ensure('firstName').required()
     .ensure('lastName').required()
