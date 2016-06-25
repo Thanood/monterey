6c4a6c103dd96655bf59a31158954fec52d99e3f
Jeroen <Jeroenvinke17@gmail.com>
2016-06-25 19:05:09 +0200

    chore(setup): revert to JSPM 0.16

--- src/main.js
+++ src/main.js
@@ -4,14 +4,7 @@ export function configure(aurelia) {
   aurelia.use
     .standardConfiguration()
     .developmentLogging()
-    .plugin('scaffolding');
+    .feature('scaffolding');
 
-  //Uncomment the line below to enable animation.
-  //aurelia.use.plugin('aurelia-animator-css');
-  //if the css animator is enabled, add swap-order="after" to all router-view elements
-
-  //Anyone wanting to use HTMLImports to load views, will need to install the following plugin.
-  //aurelia.use.plugin('aurelia-html-import-template-loader')
-
-  aurelia.start().then(() => aurelia.setRoot('monterey-shell/app'));
+  aurelia.start().then(() => aurelia.setRoot('app'));
 }
