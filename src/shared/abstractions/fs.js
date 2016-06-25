01ea9832ffff5aaf6d63b36530fe0ef8103c59f7
Jeroen <Jeroenvinke17@gmail.com>
2016-06-25 20:05:56 +0200

    chore(abstractions): use path parameter

--- src/shared/abstractions/fs.js
+++ src/shared/abstractions/fs.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 export class Fs {
   async readFile(path) {
     return new Promise((resolve, reject) => {
-      fs.readFile('node_modules/aurelia-cli/lib/commands/new/new-application.json', 'utf8', function(err, data) {
+      fs.readFile(path, 'utf8', function(err, data) {
         if (err) {
           reject(err);
         }
