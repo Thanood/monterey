6b9134a12bc54fbac35c5a85416f19c46526e334
Daniel Bendel <daniel.bendel@gmail.com>
2016-07-04 08:03:34 +0200

    refactor(jspm): load jspm plugin after npm to be able to read project config

--- src/plugins/index.js
+++ src/plugins/index.js
@@ -1,6 +1,6 @@
 export function configure(aurelia) {
   aurelia.feature('plugins/app-launcher');
-  aurelia.feature('plugins/jspm');
   aurelia.feature('plugins/npm');
+  aurelia.feature('plugins/jspm');
   aurelia.feature('plugins/project-info');
 }
--- src/plugins/jspm/index.js
+++ src/plugins/jspm/index.js
@@ -16,50 +16,10 @@ class Plugin extends BasePlugin {
   }
 
   async evaluateProject(project) {
-    let pathsToTry = [
-      `${project.path}/package.json`,
-      `${project.path}/src/skeleton-navigation-esnext-vs/package.json`,
-      `${project.path}/src/skeleton-navigation-typescript-vs/package.json`
-    ];
-
-    let found = false;
-    for (let i = 0; i < pathsToTry.length; i++) {
-      if (await this.tryLocatePackageJSON(project, pathsToTry[i])) {
-        found = true;
-      }
+    if (project.packageJSONPath) {
+      let packageJSON = JSON.parse(await FS.readFile(project.packageJSONPath));
+      await this.findJspmConfig(project, packageJSON);
     }
-
-    if (!found) {
-      if (await this.manuallyLocatePackageJSON(project)) {
-        found = true;
-      }
-    }
-
-    if (!found) {
-      project.name = FS.getDirName(project.path);
-    } else {
-      // await this.findJspmConfig(project);
-    }
-
-    return project;
-  }
-
-  async manuallyLocatePackageJSON(project) {
-    alert('Unable to find package.json, please point Monterey to package.json');
-    let paths = await FS.showOpenDialog({
-      title: 'Please select your package.JSON file',
-      properties: ['openFile'],
-      defaultPath: project.path,
-      filters: [
-        {name: 'package', extensions: ['json']}
-      ]
-    });
-
-    if (paths && paths.length > 0) {
-      return await this.tryLocatePackageJSON(project, paths[0]);
-    }
-
-    return false;
   }
 
   async findJspmConfig(project, packageJSON) {
@@ -74,27 +34,12 @@ class Plugin extends BasePlugin {
         baseURL = packageJSON.jspm.directories.baseURL;
       }
       configJs = project.path + '/' + baseURL + '/config.js';
-      console.log('configJS', configJs);
-    } else {
-      console.log('not using jspm');
     }
+
     if (configJs) {
       project.configJsPath = configJs;
     }
     project.isUsingJSPM = isUsingJSPM;
     return isUsingJSPM;
-  }
-
-  async tryLocatePackageJSON(project, p) {
-    if (await FS.fileExists(p)) {
-      project.packageJSONPath = p;
-
-      let packageJSON = JSON.parse(await FS.readFile(project.packageJSONPath));
-      project.name = packageJSON.name;
-      await this.findJspmConfig(project, packageJSON);
-      return true;
-    }
-
-    return false;
   }
 }
