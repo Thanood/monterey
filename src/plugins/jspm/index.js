6df63874e290b94aac6a9b1118f741cebf2fbdbc
Daniel Bendel <daniel.bendel@gmail.com>
2016-07-04 11:45:51 +0200

    refactor(jspm): add placeholder for JSPM 0.16/0.17 differences, output configured jspm version

--- src/plugins/jspm/index.js
+++ src/plugins/jspm/index.js
@@ -29,6 +29,24 @@ class Plugin extends BasePlugin {
     if (packageJSON.jspm) {
       isUsingJSPM = true;
 
+      let jspmDefinition = (packageJSON.dependencies && packageJSON.dependencies.jspm) || (packageJSON.devDependencies.jspm && packageJSON.devDependencies.jspm);
+      let jspmVersion = null;
+      if (jspmDefinition) {
+        jspmVersion = jspmDefinition;
+        if (jspmVersion[0] === '^' || jspmVersion[0] === '~') {
+          jspmVersion = jspmVersion.substring(1);
+        }
+        let major = parseInt(jspmVersion.split('.')[0], 10);
+        if (major < 17) {
+          // old config.js - use what we have below
+        } else {
+          // TODO: new config - read from package.json
+        }
+        project.jspmVersion = jspmDefinition;
+      } else {
+        // TODO: JSPM not found in package.json dependencies - throw error?
+      }
+
       let baseURL = '';
       if (packageJSON.jspm.directories && packageJSON.jspm.directories.baseURL) {
         baseURL = packageJSON.jspm.directories.baseURL;
@@ -40,6 +58,5 @@ class Plugin extends BasePlugin {
       project.configJsPath = configJs;
     }
     project.isUsingJSPM = isUsingJSPM;
-    return isUsingJSPM;
   }
 }
