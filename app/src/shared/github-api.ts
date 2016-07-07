a621a3589e03403829233448cc553f08a04cae1a
Jeroen <Jeroenvinke17@gmail.com>
2016-07-07 13:06:46 +0200

    feat(github): allow for user to configure github creds to remove rate limitation

--- app/src/shared/github-api.ts
+++ app/src/shared/github-api.ts
@@ -1,16 +1,48 @@
 import 'fetch';
-import {HttpClient} from 'aurelia-fetch-client';
+import {inject}        from 'aurelia-framework';
+import {HttpClient}    from 'aurelia-fetch-client';
+import {SESSION}       from 'monterey-pal';
+import {DialogService} from 'aurelia-dialog';
+import {GithubCreds}   from './github-creds';
 
+@inject(DialogService)
 export class GithubAPI {
   githubAPIUrl = 'https://api.github.com';
   client: HttpClient;
+  _authConfigured = false;
 
-  constructor() {
+  constructor(private dialogService: DialogService) {
     this.client = new HttpClient();
   }
 
-  getLatestRelease(owner, repo) {
+  async getLatestRelease(owner, repo) {
+    await this.confirmAuth();
+
     return this.client.fetch(`${this.githubAPIUrl}/repos/${owner}/${repo}/releases/latest`)
     .then(response => response.json());
+  }
+
+  async confirmAuth() {
+    if (this._authConfigured) return;
+    if (await SESSION.has('gitAuthorization')) {
+      await this.setCreds();
+    } else {
+      let response = await this.dialogService.open({ viewModel: GithubCreds });
+      if (!response.wasCancelled) {
+        await this.setCreds();
+      }
+    }
+  }
+
+  async setCreds() {
+    let authorization = await SESSION.get('gitAuthorization');
+    this.client.configure(config => {
+      config.withDefaults(<any>{ 
+        headers: {
+          'Authorization': `Basic ${authorization}`
+        }
+      });
+    });
+    this._authConfigured = true;
   }
 }
