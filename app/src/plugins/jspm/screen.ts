a158761dd06c9b12c17bd208e3e7b33b2aa113b0
Jeroen <Jeroenvinke17@gmail.com>
2016-07-08 20:57:10 +0200

    feat(jspm): package dependency list
    initial work

--- app/src/plugins/jspm/screen.ts
+++ app/src/plugins/jspm/screen.ts
@@ -3,6 +3,7 @@ import {JSPM, FS}         from 'monterey-pal';
 import {DialogService}    from 'aurelia-dialog';
 import {TaskManager}      from '../../shared/task-manager';
 import {TaskManagerModal} from '../../main/components/task-manager-modal';
+import {Analyzer}         from './analyzer';
 
 @autoinject()
 export class Screen {
@@ -10,14 +11,46 @@ export class Screen {
   lock = false;
   model;
   project;
+  a;
+  jspm;
+  config;
+  process;
+  loader;
+  topLevelDependencies = [];
+  allDependencies = [];
 
   constructor(private taskManager: TaskManager,
+              private analyzer: Analyzer,
               private dialogService: DialogService) {
   }
 
-  activate(model) {
+  async activate(model) {
     this.model = model;
     this.project = model.selectedProject;
+
+    let packageJSON = JSON.parse(await FS.readFile(this.project.packageJSONPath));
+    (<any>window)._this = this;
+    this.a = (<any>window).require;
+    let system = (<any>window).System;
+    this.jspm = this.a('jspm');
+    this.config = this.a('jspm/lib/config.js');
+    this.install = this.a('jspm/lib/install.js');
+    this.process = (<any>window).process;
+    (<any>window).System = system;
+    let originalWorkingDirectory = this.process.cwd();
+    this.process.chdir(this.project.path );
+
+    this.jspm.setPackagePath(this.project.packageJSONPath);
+    await this.config.load();
+
+    this.allDependencies = this.analyzer.analyze(this.config.loader, packageJSON);
+    this.topLevelDependencies =  this.allDependencies
+      .filter(i => i.isTopLevel)
+      .sort((a, b) => {
+        if (a.package < b.package) return -1;
+        if (a.package > b.package) return 1;
+        return 0;
+      });
   }
 
   install() {
