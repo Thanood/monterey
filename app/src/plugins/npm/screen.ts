d4be2d00b992180bc96171aca73376b0678fd86b
Jeroen <Jeroenvinke17@gmail.com>
2016-07-09 21:16:35 +0200

    feat(npm): initial npm package manager screen work

--- app/src/plugins/npm/screen.ts
+++ app/src/plugins/npm/screen.ts
@@ -1,6 +1,7 @@
 import {autoinject}       from 'aurelia-framework';
 import {Common}           from './common';
 import {DialogService}    from 'aurelia-dialog';
+import {FS}               from 'monterey-pal';
 import {TaskManager}      from '../../task-manager/task-manager';
 import {TaskManagerModal} from '../../task-manager/task-manager-modal';
 
@@ -9,6 +10,7 @@ export class Screen {
 
   model;
   project;
+  topLevelDependencies: Array<any> = [];
 
   constructor(private common: Common,
               private dialogService: DialogService) {
@@ -17,10 +19,38 @@ export class Screen {
   activate(model) {
     this.model = model;
     this.project = model.selectedProject;
-    console.log(this.project.packageJSONPath);
   }
 
-  install() {
+  attached() {
+    this.load();
+  }
+
+  async load() {
+    // let a = (<any>window).require;
+    // var npm = a('npm');
+    // await npm.load({ workingDirectory: this.project.path }, done => {
+    //   var npmLs = a('npm/lib/ls.js');
+    //   npmLs([], false, function (e) { console.log('RESPONSE:', e); });
+    // });
+
+    let packageJSON = JSON.parse(await FS.readFile(this.project.packageJSONPath));
+    let deps = Object.assign({}, packageJSON.dependencies, packageJSON.devDependencies);
+
+    // normalize data for the grid
+    let keys = Object.keys(deps);
+    for (let i = 0; i < keys.length; i++) {
+      let key = keys[i];
+      let dep = deps[key];
+      deps[key] = {
+        name: key,
+        version: deps[key]
+      };
+
+      this.topLevelDependencies.push(deps[key]);
+    }
+  }
+
+  installAll() {
     let task = this.common.installNPMDependencies(this.project);
 
     this.dialogService.open({ viewModel: TaskManagerModal, model: { task: task }});
