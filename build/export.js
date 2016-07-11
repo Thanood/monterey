cdf0df9aef00c1c128f60f15eec3178246a6f24a
Jeroen <Jeroenvinke17@gmail.com>
2016-07-11 12:41:28 +0200

    fix(deployment): deploy all node_modules files, fixes error when scaffolding aurelia-cli project

--- build/export.js
+++ build/export.js
@@ -20,9 +20,7 @@ module.exports = {
     'app/styles/styles.css',
     'app/doc/CHANGELOG.md',
     'app/scripts/**/*',
-    'app/node_modules/**/*.js',
-    'app/node_modules/**/*.json',
-    'app/node_modules/**/*.html',
+    'app/node_modules/**/*',
     'app/scripts/babel-polyfill.min.js'
   ],
   // this section lists any jspm packages that have
