43b3d7318cd5f1c4eb7349b80867c6b9d425ce15
Jeroen <Jeroenvinke17@gmail.com>
2016-07-16 18:34:21 +0200

    no message

--- travis.sh
+++ travis.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 set -ev
 if [ "${TEST_SUITE}" = "tests" ]; then
+  export CHROME_BIN=chromium-browser
+  export DISPLAY=:99.0
+  sh -e /etc/init.d/xvfb start
 	gulp test
 else
 	gulp build-release
