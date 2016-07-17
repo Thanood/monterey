de43f72909d5a9bd1caf306f622d5ed5e99aaa50
Jeroen <Jeroenvinke17@gmail.com>
2016-07-17 12:00:28 +0200

    chore(travis): added rename index task

--- travis.sh
+++ travis.sh
@@ -6,6 +6,8 @@ if [ "${TEST_SUITE}" = "tests" ]; then
   sh -e /etc/init.d/xvfb start
 	gulp test
 else
-	gulp build-release
+  gulp build-release
+  gulp rename-index
   npm run dist
+  gulp rename-index-back
 fi
