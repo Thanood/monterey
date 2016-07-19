5b3feae21f2b930b25d378199f23d5415e2cf3cc
Jeroen <Jeroenvinke17@gmail.com>
2016-07-19 14:09:49 +0200

    chore(deps): updated deps

--- custom_typings/monterey-pal.d.ts
+++ custom_typings/monterey-pal.d.ts
@@ -1,11 +1,56 @@
-declare module "monterey-pal" {
-  export var FS;
-  export var SESSION;
-  export var JSPM;
-  export var NPM;
-  export var AURELIACLI;
-  export var PROCESSES;
-  export var OS;
+declare module 'monterey-pal' {
+  export const AURELIACLI: {
+      create(model: any): void;
+      install(model: any): void;
+  };
+  export const FS: {
+      readFile(filePath: any): string;
+      fileExists(p: any): Promise<boolean>;
+      showOpenDialog(config: any): string[];
+      normalize(path: any): string;
+      writeFile(path: any, content: any): void;
+      getDirName(p: any): string;
+      getFolderPath(p: any): string;
+      join(...segments: any[]): string;
+      getTempFile(): string;
+      getRootDir(): string;
+      unzip(zipPath, outPath): void;
+      move(from, to): void;
+      getTempFolder(): string;
+      getDirectories(p: any): void;
+      cleanupTemp(): void;
+      downloadFile(url: any, targetPath: any): void;
+      _downloadFile(stream: any, url: any, targetPath: any): void;
+  };
+  export const SESSION: {
+      get(key: any): void;
+      set(key: any, value: any): void;
+      clear(): void;
+      has(key: any): void;
+  };
+  export const PROCESSES: {
+      execChildProcess(cmd: any, options?: any): void;
+  };
+  export const NPM: {
+      install(packages: any, error: any): void;
+      load(npmOptions: any, error: any): void;
+      ls(npmOptions: any): void;
+  };
+  export const JSPM: {
+      install(packages: any, options: any): void;
+      downloadLoader(options: any): void;
+      isJspmInstalled(projectPath: any): void;
+      getJSPMRootPath(projectPath: any): void;
+      getConfig(options: any): any;
+      getForks(config: any, options: any): any;
+  };
+  export const OS: {
+      getPlatform(): string;
+      getNodeVersion(): string;
+      getNPMVersion(): string;
+      getChromeVersion(): string;
+      getElectronVersion(): string;
+  };
 
-  function initializePAL(cb: (fs?, session?, aureliaCLI?, processes?, npm?, jspm?, os?) => void)
+  export function initializePAL(callback: (fs, session, aureliacli, processes, npm, jspm, os) => void): void;
 }
