06acc62001cc3ba92d1c37ada7ddbdc1be2e5674
Jeroen Vinke <JeroenVinke@users.noreply.github.com>
2016-07-26 16:40:40 +0200

    Update travis.sh

--- travis.sh
+++ travis.sh
@@ -1,1 +1,17 @@
-echo $CSC_LINK
+#!/bin/bash
+set -ev
+if [ "${TEST_SUITE}" = "tests" ]; then
+  export CHROME_BIN=/usr/bin/google-chrome
+  export DISPLAY=:99.0
+  sh -e /etc/init.d/xvfb start
+  sudo apt-get update
+  sudo apt-get install -y libappindicator1 fonts-liberation
+  wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+  sudo dpkg -i google-chrome*.deb
+	gulp test
+else
+  gulp build-release
+  gulp rename-index
+  npm run dist
+  gulp rename-index-back
+fi
