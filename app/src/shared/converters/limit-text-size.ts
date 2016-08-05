307f6ba6bd718d7106befa5ef105b2f27c4e17f6
Jeroen <Jeroenvinke17@gmail.com>
2016-08-05 13:14:14 +0200

    chore(errors): improved how long errors are displayed

--- app/src/shared/converters/limit-text-size.ts
+++ app/src/shared/converters/limit-text-size.ts
@@ -1,5 +1,7 @@
 export class LimitTextSizeValueConverter {
   toView(text: string, amountOfChars: number) {
+    if (!text) return '';
+
     if (text.length > amountOfChars) {
       return text.slice(0, amountOfChars) + '...';
     }
