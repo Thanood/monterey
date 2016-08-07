ae2e19d3825499d6f118e9b7da80592ebd822af6
vegarringdal <vegar.ringdal@gmail.com>
2016-08-07 21:56:30 +0200

    chore(terminal): terminal plugin

---
+++ app/src/plugins/terminal/index.ts
@@ -0,0 +1,21 @@
+import {PluginManager} from '../../shared/plugin-manager';
+import {BasePlugin}    from '../base-plugin';
+
+
+
+export function configure(aurelia) {
+  let pluginManager = <PluginManager>aurelia.container.get(PluginManager);
+  pluginManager.registerPlugin(aurelia.container.get(Plugin));
+}
+
+class Plugin extends BasePlugin {
+  constructor() {
+    super();
+  }
+
+  getTiles(project, showIrrelevant) {
+    return [{
+      viewModel: 'plugins/terminal/tile'
+    }];
+  }
+}
