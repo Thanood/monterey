71c937f8c83c2205835334faed879cc6483daa62
vegarringdal <vegar.ringdal@gmail.com>
2016-08-10 22:32:23 +0200

    chore(terminal): added pty.js and xterm

--- app/src/plugins/terminal/terminal-modal.ts
+++ app/src/plugins/terminal/terminal-modal.ts
@@ -1,138 +1,61 @@
 import {ApplicationState}   from '../../shared/application-state';
 import {TerminalState}      from './terminal-state';
-import {Main}               from '../../main/main';
 import {autoinject}         from 'aurelia-framework';
 import {DialogController}   from 'aurelia-dialog';
-
-declare var System: any;
-declare var process: any;
-const spawn = System._nodeRequire('child_process').spawn;
-const Convert = System._nodeRequire('ansi-to-html');
-
-
 
 @autoinject()
 export class TerminalModal {
 
-  terminal: any;
-  terminalText: string = "";
-  input: string = "";
-  path: string = "";
-  active: boolean = false;
-  convert:any;
+  constructor(private dialogController: DialogController, private state: ApplicationState, private terminalState: TerminalState) {
 
-
-  constructor(private dialogController: DialogController, private state: ApplicationState, private main: Main, private terminalState: TerminalState) {
-    this.convert = new Convert({
-      fg: '#FFF',
-      bg: '#000',
-      newline: false,
-      escapeXML: false,
-      stream: false
-    });
   }
 
-  async activate() {
-    this.path = this.main.selectedProject.path;
-    if (this.terminalState.selectedTerminal.context) {
-      this.active = true;
-    }
+  attached() {
+    //reload instance of out xterm
+    setTimeout(()=> {
+      this.terminalState.terminals.forEach((term)=> {
+        let element = document.createElement("DIV");
+        term.element.appendChild(term.xterm.element)
+      });
+    }, 100);
   }
+
 
   setTerminal(terminal) {
+    if(this.terminalState.selectedTerminal){
+      this.terminalState.selectedTerminal.active = false;
+    }
     this.terminalState.selectedTerminal = terminal;
-    this.active = true;
-    this.scroll();
+    this.terminalState.selectedTerminal.active = true;
   }
+
 
   addTerminal() {
-    this.startTerminal()
-  }
-
-  replaceAll = function (text, search, replacement) {
-    return text.replace(new RegExp(search, 'g'), replacement);
-  };
-
-  processData(terminal: any, str: string) {
-
-    str = this.replaceAll(str, ">", "&gt");
-    str = this.replaceAll(str, "<", "&lt");
-
-    terminal.terminalText = terminal.terminalText + this.convert.toHtml(str);
-
-    let textarea = document.getElementById('terminal-vr');
-    textarea.innerHTML = '<span>' + terminal.terminalText + '</span>';
-  }
-
-  startTerminal() {
-    let cmd = process.platform === 'win32' ? 'cmd' : 'bash';
-
-    let terminal = {context: null, terminalText: ""};
-    terminal.context = spawn(cmd, {
-      cwd: this.path,
-      shell: true,
-      env: Object.assign(process.env, {FORCE_COLOR: true})
-    });
-
-    terminal.context.stdout.on('data', (data) => {
-      this.processData(terminal, data.toString());
-      this.scroll();
-    });
-
-    terminal.context.stderr.on('data', (data) => {
-      this.processData(terminal, data.toString());
-      this.scroll();
-    });
-
-    terminal.context.on('exit', (code) => {
-      this.processData(terminal, 'child process exited with code ' + code + '\n');
-
-      this.terminalState.terminals.forEach((t, i)=> {
-        if (t.context.pid == terminal.context.pid) {
-          this.terminalState.terminals.splice(i, 1);
-          this.terminalState.selectedTerminal.context = null;
-          this.active = false;
-          this.scroll();
-        }
-      })
-    });
-
-    this.terminalState.terminals.push(terminal);
-    this.terminalState.selectedTerminal = terminal;
-    this.active = true;
-
+    this.generateTerminal()
   }
 
 
-  scroll() {
-    setTimeout(()=> {
-      let textarea = document.getElementById('terminal-vr');
-      if (textarea) {
-        textarea.scrollTop = textarea.scrollHeight;
-      }
-    }, 300)
+  generateTerminal() {
+    let id: string = 'terminal-' + this.terminalState.getID();
+    let element = document.createElement("DIV");
+    this.terminalState.terminals.push({
+      active: true,
+      xterm: null,
+      pty: null,
+      id: id,
+      pid: "generating",
+      element: element
+    });
+
+    setTimeout((e)=> {
+      this.terminalState.createPTY(id);
+    }, 200)
   }
 
 
   closeTerminal() {
-    if (this.terminalState.selectedTerminal.context) {
-      this.terminalState.killProcess(this.terminalState.selectedTerminal.context.pid);
-    }
-  }
-
-
-  keydown(e) {
-    //listen for contrl+c
-    if (e.ctrlKey && e.keyCode === 67) {
-      this.closeTerminal();
-    }
-
-    if (e.keyCode === 13) {
-      this.terminalState.selectedTerminal.context.stdin.write(`${this.input}\n`);
-      this.input = "";
-      return false;
-    } else {
-      return true;
+    if (this.terminalState.selectedTerminal) {
+      this.terminalState.selectedTerminal.pty.kill();
     }
   }
 
--- app/src/plugins/terminal/terminal-state.ts
+++ app/src/plugins/terminal/terminal-state.ts
@@ -1,24 +1,110 @@
-declare var System: any;
-const treeKill = System._nodeRequire('tree-kill');
+import {Main}               from '../../main/main';
+import {autoinject}         from 'aurelia-framework';
 
+declare var System: any;
+declare var process: any;
+
+const PTY = System._nodeRequire('pty.js');
+const XTERM = System._nodeRequire("xTerm");
+
+@autoinject()
 export class TerminalState {
   terminals: any;
   selectedTerminal: any;
+  id: number = 0;
+  path:string;
 
-  constructor() {
+  constructor(private main: Main) {
     this.terminals = [];
     this.selectedTerminal = {};
+    this.path = this.main.selectedProject.path;
   }
 
-  killProcess(pid) {
-    treeKill(pid, 'SIGKILL');
+  getID() {
+    return this.id++;
   }
 
-  //this need to be called when monetery quit!
-  killAll() {
-    this.terminals.forEach((terminal)=> {
-      this.killProcess(terminal.context.pid);
-    })
+
+  createXterm(id) {
+
+    let length = this.terminals.length;
+
+    let xterminal = new XTERM({
+      cursorBlink: true,
+      cols: 70,
+      rows: 18
+    });
+    let element = document.createElement("DIV");
+    xterminal.open(this.terminals[length - 1].element);
+
+    xterminal.on('key', (key, ev)=> {
+      var printable = (
+        !ev.altKey && !ev.altGraphKey && !ev.ctrlKey && !ev.metaKey
+      );
+
+      if (ev.keyCode == 13) {
+        this.selectedTerminal.pty.write(key)
+      } else if (ev.keyCode == 8) {
+
+        // Do not delete the prompt
+        let children = xterminal.rowContainer.children;
+        let length = xterminal.rowContainer.children.length;
+
+        if (xterminal.x > children[length - 1].innerText.length) {
+          this.selectedTerminal.pty.write('\b \b');
+        }
+
+      } else if (printable) {
+        this.selectedTerminal.pty.write(key)
+      }
+    });
+
+    xterminal.on('paste', function (data, ev) {
+      this.selectedTerminal.pty.write(data);
+    });
+
+    return xterminal;
+
+  }
+
+
+  createPTY(id) {
+    let terminalView = this.createXterm(id);
+    let cmd = process.platform === 'win32' ? process.env['comspec'] || 'cmd.exe' : process.env.SHELL || 'sh';
+
+    var ptyTerminal = PTY.spawn(cmd, [], {
+      name: 'xterm-color',
+      env: process.env,
+      cwd: this.path,
+      cols: terminalView.cols
+    });
+
+    ptyTerminal.on('data', (data)=> {
+      terminalView.write(data)
+    });
+
+    ptyTerminal.on('exit', (code) => {
+      this.terminals.forEach((t, i)=> {
+        if (t.pty.pid === this.selectedTerminal.pty.pid) {
+          this.terminals.splice(i, 1);
+          this.selectedTerminal = null;
+         // this.active = false;
+        }
+      })
+    });
+
+    let length = this.terminals.length;
+
+    this.terminals[length - 1].xterm = terminalView;
+    this.terminals[length - 1].pty = ptyTerminal;
+
+    if(this.selectedTerminal){
+      this.selectedTerminal.active = false;
+    }
+    this.selectedTerminal = this.terminals[length - 1];
+
+    //this.active = true;
+
   }
 
 }
