90adb271de77681bce23b00e0a6c25d7fb22de07
JeroenVinke <jeroenvinke17@gmail.com>
2016-09-12 17:57:53 +0200

    chore(tooltip): import via globalResources

--- app/src/plugins/npm/tile.ts
+++ app/src/plugins/npm/tile.ts
@@ -9,7 +9,7 @@ import {NPMDetection}        from './npm-detection';
 export class Tile {
   title: string;
   img: string;
-  tooltip = 'npm-tooltip';
+  tooltip = 'tooltip-npm';
 
   constructor(private main: Main,
               private selectedProject: SelectedProject,
